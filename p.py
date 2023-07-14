86dce2790c96bc1e7d427a018af398df364e4001
faritg1 <juanagelves@gmail.com>
2023-07-13 23:19:06 -0500

    faltafaltfalta

--- p.py
+++ p.py
@@ -25,5 +25,35 @@ especiesAnimales = {
     "ave":["gallinas","pavos","gansos","palomas","loros"]
 }
 
-for i,item in enumerate(especiesAnimales.values()):
+for i,item in enumerate(especiesAnimales.keys()): # keys, items, values
     print(f'{i+1}. {item}')
+
+especie = int(input(": "))
+
+print(item)
+print("")
+
+if especie == 1:
+    print("PERRO")
+    for i,item in enumerate(especiesAnimales.values()):
+        print(f'{i+1}. {item}')
+    raza = int(input(": "))
+    especieRaza = "perros"
+elif especie == 2:
+    print("GATO")
+    for i,item in enumerate(especiesAnimales.values()):
+        print(f'{i+1}. {item}')
+    raza = int(input(": "))
+    especieRaza = "gatos"
+elif especie == 3:
+    print("reptil")
+    for i,item in enumerate(especiesAnimales.values()):
+        print(f'{i+1}. {item}')
+    raza = int(input(": "))
+    especieRaza = "reptil"
+elif especie == 4:
+    print("ave")
+    for i,item in enumerate(especiesAnimales.values()):
+        print(f'{i+1}. {item}')
+    raza = int(input(": "))
+    especieRaza = "ave"
